8efd17c708267bef066b28ba8ca6830feb77aec7
jonsoku2 <whdtjr2792@gmail.com>
2020-03-27 21:11:50 +0900

    리덕스 예제 - 카운터

--- src/pages/Home/HomeContainer.jsx
+++ src/pages/Home/HomeContainer.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import HomePresenter from './HomePresenter';
 
-const HomeContainer = () => {
+import { connect } from 'react-redux';
+
+const HomeContainer = props => {
+  console.log(props);
   return (
     <div>
-      <HomePresenter />
+      <h1>
+        Current Number : <strong>{props.counter}</strong>
+        <div>
+          <button onClick={() => props.increment()}>Increment</button>
+          <button onClick={() => props.decrement()}>Decrement</button>
+        </div>
+      </h1>
     </div>
   );
 };
 
-export default HomeContainer;
+const mapToStateProps = state => {
+  return {
+    counter: state.counter,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increment: () => dispatch({ type: 'INCREASE' }),
+    decrement: () => dispatch({ type: 'DECREASE' }),
+  };
+};
+
+export default connect(mapToStateProps, mapDispatchToProps)(HomeContainer);
---
+++ src/store/reducers/counter.reducer.js
@@ -0,0 +1,12 @@
+const initialState = 0;
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREASE':
+      return state + 1;
+    case 'DECREASE':
+      return state - 1;
+    default:
+      return state;
+  }
+};
