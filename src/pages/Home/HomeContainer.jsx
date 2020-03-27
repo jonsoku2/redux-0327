dc17996021db75d421112471c70b588ce3fce433
jonsoku2 <whdtjr2792@gmail.com>
2020-03-27 22:00:24 +0900

    리팩토링 2

--- src/pages/Home/HomeContainer.jsx
+++ src/pages/Home/HomeContainer.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { increase, decrease } from '../../store/actions/counter.action';
 import { connect } from 'react-redux';
+import { increase, decrease } from '../../store/actions/counter.action';
+import HomePresenter from './HomePresenter';
 
-const HomeContainer = ({ counter, increase, decrease }) => {
-  return (
-    <div>
-      <h1>
-        Current Number : <strong>{counter}</strong>
-        <div>
-          <button onClick={increase}>increase</button>
-          <button onClick={decrease}>decrease</button>
-        </div>
-      </h1>
-    </div>
-  );
-};
-
-HomeContainer.propTypes = {
-  counter: PropTypes.number.isRequired,
-  increase: PropTypes.func.isRequired,
-  decrease: PropTypes.func.isRequired,
+const HomeContainer = props => {
+  return <HomePresenter {...props} />;
 };
 
 const mapToStateProps = ({ counter }) => ({
--- src/pages/Home/HomePresenter.jsx
+++ src/pages/Home/HomePresenter.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const HomePresenter = () => {
-  return <div>HomePresenter</div>;
+const HomePresenter = ({ counter, increase, decrease }) => {
+  return (
+    <div>
+      <h1>
+        Current Number : <strong>{counter}</strong>
+        <div>
+          <button onClick={increase}>increase</button>
+          <button onClick={decrease}>decrease</button>
+        </div>
+      </h1>
+    </div>
+  );
+};
+
+HomePresenter.propTypes = {
+  counter: PropTypes.number.isRequired,
+  increase: PropTypes.func.isRequired,
+  decrease: PropTypes.func.isRequired,
 };
 
 export default HomePresenter;
